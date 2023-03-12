4de32ac5111cf4edf77c5b20446dc41df88a7dd0
Francis Masha <francismasha96@gmail.com>
2023-03-12 07:36:01 +0300

    feat: add elasticsearch client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+use elasticsearch::cat::CatIndicesParts;
+use elasticsearch::Elasticsearch;
+use elasticsearch::http::transport::Transport;
+use serde_json::Value;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let transport = Transport::single_node("http://10.197.31.59:9200")?;
+    let client = Elasticsearch::new(transport);
+
+    let response = client
+      .cat()
+      .indices(CatIndicesParts::Index(&["*"]))
+      .send()
+      .await?;
+
+    let response_body = response.json::<Value>().await?;
+
+    for record  in response_body.as_array().unwrap() {
+        println!("{}", record["index"].as_str().unwrap());
+    }
+
     println!("Hello, world!");
+
+    Ok(())
 }
